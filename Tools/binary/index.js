195dd6ab27f2ca565ea11d050a02c156c3e84dfd
Zachary Gopinath <zachary.gopinath@GCCYJPRUJ1WK.local>
2020-09-21 13:45:42 -0400

    Finished Binary Website

--- Tools/binary/index.js
+++ Tools/binary/index.js
@@ -1,12 +1,24 @@
 function calculate(){
     var num = document.getElementById("input").value;
-    var n = 0;
-    var total = 0;
-    for (i = num.length - 1; i >= 0; i--){
-        total = total + parseInt(num[i]) * (2**n);
-        n = n + 1
-        console.log(total)
-    };
-   var finalTotal = document.getElementById("outputBox").append(total)
-
+    var isValid = true;
+    for (var i  = 0; i < num.length; ++i){
+        if (num[i] == 1 || num[i] == 0){
+            console.log(num[i] + " == Good");
+        }
+        else{
+            isValid = false;
+        }
+    }
+    if (isValid == true){
+        var n = 0;
+        var total = 0;
+        for (i = num.length - 1; i >= 0; i--){
+            total = total + parseInt(num[i]) * (2**n);
+            n = n + 1;
+        }
+       document.getElementById("outputBox").innerHTML = total;
+    }
+    else{
+        document.getElementById("outputBox").innerHTML = "Not a Valid Number"
+    }
 }
